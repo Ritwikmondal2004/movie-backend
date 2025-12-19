467f55356fd5b56bf5a63137e17142341a47f67c
Ritwikmondal2004 <ritwik8575@gmail.com>
2025-12-19 11:52:26 +0530

    Add response body utility for success and error handling

---
+++ backend/src/utils/responseBody.js
@@ -0,0 +1,19 @@
+const errorResponseBody = {
+  err: {},
+  data: {},
+  message: "something went wrong",
+  success: false,
+};
+const successResponseBody = {
+  err: {},
+  data: {},
+  message: "Sucessfully fetched movie details",
+  sucess: true,
+};
+
+
+module.exports={
+    successResponseBody,
+    errorResponseBody
+
+}
